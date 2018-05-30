6ceb2c6f3d555b2c98d86ad963667153d1b8ead1
pelle <pellep@sina.com>
2018-05-30 23:46:42 +0800

    print

--- controllers/wechat.go
+++ controllers/wechat.go
@@ -6,7 +6,6 @@ import (
 	"crypto/sha1"
 	"encoding/hex"
 	"strings"
-	"fmt"
 )
 
 type WechatController struct{
@@ -24,15 +23,13 @@ func (c *WechatController)Entry() {
 	timestamp := c.GetString("timestamp")
 	nonce := c.GetString("nonce")
 	echostr := c.GetString("echostr")
-	fmt.Println("1:"+signature)
-	fmt.Println("2:"+timestamp)
-	fmt.Println("3:"+nonce)
-	fmt.Println("4:"+echostr)
 
 	keys := []string{token,timestamp,nonce}
 	sort.Strings(keys)
 	sign := strings.Join(keys,"")
-	if hex.EncodeToString(sha1.New().Sum([]byte(sign))) != signature{
+	strsha := sha1.New()
+	strsha.Write([]byte(sign))
+	if hex.EncodeToString(strsha.Sum(nil)) != signature{
 		c.Ctx.WriteString("Bad Gate Way")
 	} else {
 		c.Ctx.WriteString(echostr)
